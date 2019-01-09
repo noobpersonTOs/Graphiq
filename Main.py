41c987045a3f2e9910f7096c18e887242611cc98
Graphiq123 <46511319+Graphiq123@users.noreply.github.com>
2019-01-08 19:38:30 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,22 +16,19 @@ url="https://twitch.tv/celabrat", type=1))
 
 @bot.event
 async def on_message(message):
-  if message.content.startswith('hello'):
-    msg = 'Hello {0.author.mention}'.format(message)
+	if message.content.startswith('hello'):
+		msg = 'Hello {0.author.mention}'.format(message)
 		await bot.send_message(message.channel, msg)
 	if message.content.startswith('.welcome'):
 		msg = 'Thanks for having me in your server {0.author.mention}!'.format(message)
 		await bot.send_message(message.channel, msg)
 	if message.content.startswith('.ping'):
-		userID = message.author.id
-		await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
+		await bot.send_message(message.channel, "<@%s> Pong!".format(message))
 	if message.content.startswith("inviteme"):
-		userID = message.author.id
-		msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format((message))
-		await bot.send_message(message.channel, msg)
+		msg = "https://discordapp.com/api/oauth2/authorize?client_id=529463184910712872&permissions=0&scope=bot {0.author.mention}".format(message)
+		await bot.send_message(message.channel, msg))
 	if message.content.startswith('.adminme'):
-		userID = message.author.id
-		await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>" % (userID))
+		await bot.send_message(message.channel, ":x: You do not have the permission to do that <@%s>".format(message))
 	await bot.process_commands(message)
 
 @bot.command()
